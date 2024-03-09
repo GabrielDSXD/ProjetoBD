f176670cb63b585ffc510a351a8eb636795bcfa5
Gabriel D <gabrieldantasousa1@gmail.com>
2024-03-09 11:42:35 -0300

    ..

---
+++ src/main/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import Header from "../components/Header/index";
+import style from "./style.module.css";
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
